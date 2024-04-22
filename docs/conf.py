e3ef00577e6fdc49901a6dd9d00a0697462e2785
Flynn OConnell <Flynnoconnell@gmail.com>
2024-04-22 14:58:57 -0400

    add matlab_src_dir to apidoc

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+import os
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
+matlab_src_dir = os.path.abspath("..")
 
 project = 'caiman_matlab'
 copyright = '2024, Elizabeth R. Miller Brain Observatory (MBO)'
-
-extensions = [
-]
 
 source_suffix = {
         '.rst': 'restructuredtext',
@@ -29,42 +13,11 @@ source_suffix = {
         '.md': 'markdown',
         }
 
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-import sys, os
-sys.path.insert(0, os.path.abspath('.'))
-exclude_patterns = ['zzz']
-
-numfig = True
-
-extensions.append('sphinx.ext.todo')
-extensions.append('sphinx.ext.autodoc')
-extensions.append('numpydoc')
-#extensions.append('sphinx.ext.autosummary')
-extensions.append('sphinx.ext.intersphinx')
-extensions.append('sphinx.ext.mathjax')
-extensions.append('sphinx.ext.viewcode')
-extensions.append('sphinx.ext.graphviz')
-
-
-html_theme = 'default'
+extensions = ["sphinx.ext.autodoc", "sphinxcontrib.matlab", "numpydoc", "sphinx.ext.intersphinx"]
+primary_domain = "mat"
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "default"
