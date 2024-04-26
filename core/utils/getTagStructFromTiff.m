4f9e699ea4f74cd5024c1df9343ad82a013db811
= <=>
2024-04-26 10:47:29 -0400

    add bitspersample/sampleformat to tiff tag struct

--- core/utils/getTagStructFromTiff.m
+++ core/utils/getTagStructFromTiff.m
@@ -1,13 +1,16 @@
 function [tagstruct] = getTagStructFromTiff(hTiff)
 
     tagstruct = struct();
-    tagstruct.BitsPerSample = 16;
+    tagstruct.BitsPerSample = hTiff.getTag("BitsPerSample");
     tagstruct.Photometric = hTiff.getTag("Photometric");
     tagstruct.Orientation = hTiff.getTag("Orientation");
     tagstruct.SubFileType = hTiff.getTag("SubFileType");
 
+    tagstruct.Compression = hTiff.getTag("Compression");
+    tagstruct.RowsPerStrip = hTiff.getTag("RowsPerStrip");
     tagstruct.SamplesPerPixel = hTiff.getTag("SamplesPerPixel");    
-    tagstruct.SampleFormat = Tiff.SampleFormat.UInt;
+    tagstruct.SampleFormat = hTiff.getTag("SampleFormat");
+
     tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
     tagstruct.ImageLength = hTiff.getTag("ImageLength");
     tagstruct.ImageWidth = hTiff.getTag("ImageWidth");
