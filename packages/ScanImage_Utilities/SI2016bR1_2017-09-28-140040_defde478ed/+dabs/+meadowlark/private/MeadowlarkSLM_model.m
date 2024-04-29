d79cdb9c89a98616976fb2d8edbc1e1095898efd
= <=>
2024-04-29 15:35:23 -0400

    move to ScanImage 2019

--- packages/ScanImage_Utilities/SI2016bR1_2017-09-28-140040_defde478ed/+dabs/+meadowlark/private/MeadowlarkSLM_model.m
+++
@@ -1,3 +0,0 @@
-%% MeadowlarkSLM
-pixelResolutionXY = [512,512];  % [1x2 numeric] pixel resolution of SLM
-pixelPitchXY      = [15 15];    % [1x2 numeric] distance from pixel center to pixel center in microns
--- packages/ScanImage_Utilities/SI2016bR1_2017-09-28-140040_defde478ed/+scanimage/+components/private/IntegrationRoiManager_model.m
+++
@@ -1,4 +0,0 @@
-%% IntegrationRoiOutputs
-channelNames = {};                      % User defined name for each output channels e.g. {'My first channel','My second channel','My third channel'}
-deviceNames = {};                       % Names of DAQ device for each channel, use 'none' if no physical device is used e.g. {'Dev1','Dev1','none'}
-deviceChannels = {};                    % Channel index for each channel: numeric values for analog outputs, strings for digital outputs, use 'none' for software output; e.g {0, 'port0/line0', 'none'} for AO0, port0/line0 and a software output
