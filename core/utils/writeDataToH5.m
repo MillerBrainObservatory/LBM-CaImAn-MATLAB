a551ac3df58ffbb16e3511a890657add25dad999
= <=>
2024-05-10 19:32:10 -0400

    bugfix: overwriting h5 groups, add overwrite nvarg

--- core/utils/writeDataToH5.m
+++ core/utils/writeDataToH5.m
@@ -1,19 +1,34 @@
-function writeDataToH5(data, filePath, datasetPath, nvargs, metadata)
-    try
-        info = h5info(filepath, datasetPath);
-    catch
-       fprintf("%s doesnt exists, creating...\n", datasetPath);
-       h5create(filePath, datasetPath, size(data), 'Datatype', 'uint16', ...
-         'ChunkSize', [size(data, 1), size(data, 2), 1], 'Deflate', nvargs.compression);
-    end
-   
-    % Write the data to the specified dataset within the group
+function writeDataToH5(data, metadata, filePath, datasetPath, nvargs)
+% if the file doesnt exist, we know to create both file + dataset
+if ~isfile(filePath)
+    fprintf("%s does not exist, Creating the h5 file... \n", filePath)
+    h5create(filePath, datasetPath, size(data), 'Datatype', 'uint16', ...
+     'ChunkSize', [size(data, 1), size(data, 2), 1], 'Deflate', nvargs.compression);  
     h5write(filePath, datasetPath, data);
+else
+    % file exists, check that the dataset exists
+    try
+        h5create(filePath, datasetPath, size(data), 'Datatype', 'uint16', ...
+            'ChunkSize', [size(data, 1), size(data, 2), 1], 'Deflate', nvargs.compression); 
+        h5write(filePath, datasetPath, data);
+    catch ME  % errors if the file does exist
+        if nvargs.overwrite == 1
+            fprintf('File: %s \n ...with dataset-path: %s already exists, but user input overwrite = 1, writing over the file...', filePath, datasetPath);
 
-    % Write metadata attributes to the dataset
-    fields = string(fieldnames(metadata));
-    for f = fields'
-        h5writeatt(filePath, datasetPath, f, metadata.(f));
+            h5write(filePath, datasetPath, data);  % overwrite the data
+        else
+            fprintf('File: %s \n ...with dataset-path: %s already exists, skipping this file...', filePath, datasetPath);
+        end
     end
 end
+
+% Write the data to the specified dataset within the group
+
+
+% Write metadata attributes to the dataset
+fields = string(fieldnames(metadata));
+for f = fields'
+    h5writeatt(filePath, datasetPath, f, metadata.(f));
+end
+
 
