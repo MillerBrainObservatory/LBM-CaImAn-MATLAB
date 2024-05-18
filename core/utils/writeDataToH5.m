5610bc052b4a995732f42436ac6e9c37076aad86
= <=>
2024-05-18 12:12:17 -0400

    bugfix: allow h5 overwrite if file exists and parameters changed

--- core/utils/writeDataToH5.m
+++ core/utils/writeDataToH5.m
@@ -14,8 +14,10 @@ else
     catch ME  % errors if the file does exist
         if nvargs.overwrite == 1
             fprintf('File: %s \n ...with dataset-path: %s already exists, but user input overwrite = 1, writing over the file...', filePath, datasetPath);
-
-            h5write(filePath, datasetPath, data);  % overwrite the data
+            delete(filePath);
+            h5create(filePath, datasetPath, size(data), 'Datatype', 'uint16', ...
+             'ChunkSize', [size(data, 1), size(data, 2), 1], 'Deflate', nvargs.compression);  
+            h5write(filePath, datasetPath, data);
         else
             fprintf('File: %s \n ...with dataset-path: %s already exists, skipping this file...', filePath, datasetPath);
         end
