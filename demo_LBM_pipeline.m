d573c8a8af38557ede2a4ccccde9025e8b32990b
Flynn OConnell <flynnoconnell@gmail.com>
2024-05-24 12:46:52 -0400

    chore: consistent naming and documentation

--- demo_LBM_pipeline.m
+++ demo_LBM_pipeline.m
@@ -21,9 +21,8 @@ clc, clear;
 [fpath, fname, ~] = fileparts(fullfile(mfilename('fullpath'))); % path to this script
 addpath(genpath(fullfile(fpath, 'core/')));
 
-%% Here you can validate that all packages are on the path and accessible
-% from within this pipeline.
-
+%% Here you can validate that all dependencies are on the path and accessible from within this pipeline.
+%% This does not check for package access on your path.
 result = validateRequirements();
 if ischar(result)
     error(result);
@@ -31,56 +30,30 @@ else
     disp('Proceeding with execution...');
 end
 
-parent_path = 'C:\Users\RBO\Documents\data\bi_hemisphere\';
+parent_path = 'C:\Users\<Username>\Documents\data\bi_hemisphere\';
 raw_path = [ parent_path 'raw\'];
-extraction_path = [ parent_path '\extracted_final\'];
-registration_path = [ parent_path 'registration_final\'];
-segmentation_path = [ parent_path 'traces_final\'];
- mkdir(registration_path); mkdir(segmentation_path);
+extraction_path = [ parent_path '\extracted\'];
+registration_path = [ parent_path 'registration\'];
+segmentation_path = [ parent_path 'traces\'];
 
 %% 1a) Pre-Processing
-if ~isfolder(extraction_path)
-    mkdir(extraction_path);
-end
 group_path = "/extraction"; % where data is saved in the h5 file (this is default)
 convertScanImageTiffToVolume( ...
-    raw_path, ...
-    extraction_path, ...
-    'group_path', group_path, ...
-    'fix_scan_phase', false, ...
-    'trim_pixels', [8 8 29 0], ...
+    raw_path, ... %% where our .tif files live
+    extraction_path, ... %% our output filepath
+    'group_path', group_path, ...  %% What we name our output group
+    'fix_scan_phase', true, ...
+    'trim_pixels', [7 7 29 0], ...
     'overwrite', 1 ...
     );
 
-%% quick vis
-plane = 1;
-frame_start = 10;
-frame_end = 220;
-
-h5files = dir([extraction_path '*.h5']);
-h5name = fullfile(extraction_path, h5files(1).name);
-dataset_path = sprintf('/extraction/plane_%d', plane);
-has_mc(h5name)
-data = h5read( ...
-    h5name, ... % filename
-    dataset_path, ... % dataset location
-    [1, 1, frame_start], ... % start index for each dimension [X,Y,T]
-    [Inf, Inf,  frame_end - frame_start + 1] ... % count for each dimension [X,Y,T]
-    );
-
-% 
-% figure;
-% for x = 1:size(data, 3)
-%     imshow(data(236:408, 210:377, x), []);
-%     title(sprintf('Frame %d', start_frame + x - 1));
-% end
 
 %% 1b) Motion Correction
 
 % mdata = get_metadata(fullfile(raw_path ,"MH184_both_6mm_FOV_150_600um_depth_410mW_9min_no_stimuli_00001_00001.tif"));
 % mdata.base_filename = "MH184_both_6mm_FOV_150_600um_depth_410mW_9min_no_stimuli_00001";
 % mc_path_new = [ parent_path 'registration_test\'];
-% 
+%
 if ~isfolder(registration_path)
     mkdir(registration_path);
 end
@@ -96,9 +69,9 @@ motionCorrectPlane( ...
     );
 
 % %% 2) CNMF Plane-by-plane SegmentationS
-% 
+%
 % segmentPlane(mc_path, traces_path, mdata, '0','30','30','23');
-% 
+%
 % %% 3) Axial Offset Correction
 % collatePlanes()
 
