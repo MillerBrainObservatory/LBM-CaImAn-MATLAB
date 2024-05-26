1aa10aee4a4f10a54c76738d8b60a1fbc446cec0
= <=>
2024-05-26 00:16:38 -0400

    chore: add dataset name to chunked h5 save

--- core/io/write_chunk_h5.m
+++ core/io/write_chunk_h5.m
@@ -1,12 +1,11 @@
-function write_chunk_h5(file,Y_in, chunk)
-
-% rewrites a file into a hdf5 file
-if ~exist('chunk','var') || isempty(chunk); chunk = 2000; end
+function write_chunk_h5(file,Y_in, chunk, dataset_name)
+if ~exist('dataset_name','var') || isempty(dataset_name)dataset_name = '/extracted';end
+if ~exist('chunk','var') || isempty(chunk) chunk = 2000; end
 keep_reading = true;
 cl = class(Y_in);
 nd = ndims(Y_in) - 1;
 sizY = size(Y_in);
-d = prod(sizY(1:nd));
+
 if sizY(end) < chunk+1
     keep_reading = false;
 else
@@ -19,8 +18,8 @@ else
 end
 [pathstr, name, ~] = fileparts(file);
 h5_filename = [pathstr,'/',name,'.h5'];
-h5create(h5_filename,'/mov',[sizY(1:nd),Inf],'Chunksize',[sizY(1:nd),min(chunk,sizY(end))],'Datatype',cl);
-h5write(h5_filename,'/mov',Y_in,[ones(1,nd),1],sizY);
+h5create(h5_filename,dataset_name,[sizY(1:nd),Inf],'Chunksize',[sizY(1:nd),min(chunk,sizY(end))],'Datatype',cl);
+h5write(h5_filename,dataset_name,Y_in,[ones(1,nd),1],sizY);
 cnt = sizY(end);
 while keep_reading
     Y_in = read_file(file,cnt+1,chunk+1);
@@ -35,6 +34,6 @@ while keep_reading
         end
         sizY(end) = sizY(end)-1;
     end
-    h5write(h5_filename,'/mov',Y_in,[ones(1,nd),cnt+1],sizY);
+    h5write(h5_filename,dataset_name,Y_in,[ones(1,nd),cnt+1],sizY);
     cnt = cnt + sizY(end);
 end
