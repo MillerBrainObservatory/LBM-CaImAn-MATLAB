2d105f105add9382854d055a3470acbb6b22567b
= <=>
2024-05-28 22:24:02 -0400

    bugfix: attribute name leading /

--- core/io/read_h5_metadata.m
+++ core/io/read_h5_metadata.m
@@ -3,7 +3,7 @@ function metadata = read_h5_metadata(h5_fullfile, loc)
     metadata = struct();
     for k = 1:numel(h5_data.Attributes)
         attr_name = h5_data.Attributes(k).Name;
-        attr_value = h5readatt(h5_fullfile, h5_data.Name, attr_name);
+        attr_value = h5readatt(h5_fullfile, ['/' h5_data.Name], attr_name);
         metadata.(matlab.lang.makeValidName(attr_name)) = attr_value;
     end
 end
