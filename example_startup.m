b9f9158ee3a3bc704e89b4bde7725136ccdf0a7a
Flynn <75346097+FlynnOConnell@users.noreply.github.com>
2024-06-09 20:58:28 -0400

    Update example_startup.m

--- example_startup.m
+++ example_startup.m
@@ -1,4 +1,9 @@
 %% This file should go in your matlab path
-cd C:/Users/RBO/repos/LBM_caiman_matlab/;
+% >> userpath
+% ans = 
+%      'C:/<username>/Documents/MATLAB'
+
+% Tell MATLAB to open where our code is located
+cd <path to LBM_caiman_matlab>;
 addpath(genpath(fullfile(cd, "core/")));
 addpath(genpath(fullfile(cd, "packages/")));
