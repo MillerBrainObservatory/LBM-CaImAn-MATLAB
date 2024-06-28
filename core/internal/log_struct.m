06d2a00db19f041f79d9417f4c578e0602f7bc9d
FlynnOConnell <FlynnOConnell@gmail.com>
2024-06-28 13:03:40 -0400

    chore: correct file locations

--- core/internal/log_struct.m
+++ core/internal/log_struct.m
@@ -1,25 +1,30 @@
-function log_metadata(metadata, logFilePath, fid)
-% LOG_METADATA Print metadata to a log file in tabular format.
+function log_struct(structured_arr,label,logFilePath, fid)
+% LOG_STRUCT Print struct to a log file in tabular format.
 %
 % Parameters
 % ----------
-% metadata : struct
+% structured_arr : struct
 %     Metadata structure to be printed.
+% label : char
+%     Title label to print above the log entry.
 % logFilePath : char
 %     Path to the log file.
+% fid : single
+%     Filepath identifyer for this logfile.
 
 % Open the log file for writing
 % TODO: Append to existing/partial logfile
 if ~exist('fid', 'var') || length(nargin) < 3; fid = fopen(logFilePath, 'a'); end
 if fid == -1; error('Cannot open log file: %s', logFilePath); end
 try
-    fprintf(fid, 'Metadata:\n');
+    fprintf(fid, '\n\n');
+    fprintf(fid, '%s\n', label);
     fprintf(fid, '-----------------------------------------------------\n');
     
-    fields = fieldnames(metadata);
+    fields = fieldnames(structured_arr);
     for i = 1:numel(fields)
         field = fields{i};
-        value = metadata.(field);
+        value = structured_arr.(field);
         
         if isnumeric(value)
             valueStr = mat2str(value);
@@ -32,7 +37,6 @@ try
         end
         fprintf(fid, '%-30s: %s\n', field, valueStr);
     end
-    
     fprintf(fid, '-----------------------------------------------------\n');
 catch ME
     rethrow(ME);
