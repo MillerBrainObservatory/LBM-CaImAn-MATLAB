cb452138bdae62d5d8b5057cfcf57e6585ca5e0a
FlynnOConnell <FlynnOConnell@gmail.com>
2024-07-03 20:29:16 -0400

    feat: read_plane accepts matfiles, any arg combo, better docstring

--- core/utils/read_plane.m
+++ core/utils/read_plane.m
@@ -1,24 +1,24 @@
 function Y_out = read_plane(varargin)
-% READ_PLANE Reads the specified frames from a dataset in a folder or file.
-%
+% Read a specific plane from an HDF5 or MAT file and return the data. If
+% input is a MAT file, a matfile object is returned.
+% 
 % Parameters
 % ----------
-% path : char or string, optional
-%     The path to the folder containing plane files or a fully qualified
-%     path to an HDF5 file.
-% dataset_name : char or string
-%     The name of the dataset within the HDF5 file (e.g., '/Y').
-% plane_number : int
-%     The plane number to read from the folder. This is ignored if `folder` is a file path.
-% n_frames : array of int, char, string, scalar, optinal
-%     The specific frames to read from the dataset.
-%     Can be a slice (e.g. 1:100). Can be a scalar (e.g. 2). Or, for all
-%     frames, 'all' or "all". Default is 'all'.
-%
+% path : str, optional
+%     Path to the HDF5 or MAT file or the directory containing plane files. If empty, a file selection dialog will open.
+% ds : str, optional
+%     Dataset group name within the HDF5 file. If empty, the function will attempt to find a valid dataset or prompt the user.
+% plane : int, optional
+%     Plane number to read from the file. User is prompted if not given.
+% frames : str or int, optional
+%     Frames to read from the dataset. Can be 'all', a scalar frame number
+%     i.e. 2, a slice i.e. 1:400, vector of size 2 with the start and stop
+%     index i.e. [5 400]. Default is 'all'.
+% 
 % Returns
 % -------
-% Y_out : array
-%     The read data from the specified frames of the dataset.
+% Y_out : array-like or MAT-file object
+%     The data read from the specified plane and frames in the file.
 %
 % Notes
 % -----
@@ -28,8 +28,14 @@ function Y_out = read_plane(varargin)
 %
 % Examples
 % --------
+% Use dialog and prompts only:
+%     Y_out = read_plane();
+% Read all frames from a file givin the full filepath:
+%     Y_out = read_plane('data.h5');
+% Use plane argument to read from a folder of files with _plane_ in the name:
+%     Y_out = read_plane('C:/data/extracted_files', 'plane', 4);
 % Read specific frames from a dataset in a folder:
-%     Y_out = read_plane('path/to/folder', '/Y', 1, 1:10);
+%     Y_out = read_plane('data.h5', 4, 'all');
 %
 % Read specific frames from a dataset in a file:
 %     Y_out = read_plane('path/to/file.h5', '/Y', [], 1:10);
@@ -39,58 +45,70 @@ function Y_out = read_plane(varargin)
 % h5info, h5read
 
 p = inputParser;
-addOptional(p, 'path', "None", @(x) (ischar(x) || isstring(x)));
-addParameter(p, 'dataset_name', "/Y", @(x) (ischar(x) || isstring(x)) && is_valid_group(x));
-addParameter(p, 'plane_number', @isnumeric);
-addParameter(p, 'n_frames', 'all', @(x) (ischar(x) || isstring(x)) || isscalar(x));
+addOptional(p, 'path', '', @(x) (ischar(x) || isstring(x)));
+addOptional(p, 'ds', '', @(x) (ischar(x) || isstring(x)) && is_valid_group(x));
+addParameter(p, 'plane', 0, @isnumeric);
+addParameter(p, 'frames', 'all', @(x) (ischar(x) || isstring(x)) || isscalar(x));
 parse(p, varargin{:});
 
 path = p.Results.path;
-dataset_name = p.Results.dataset_name;
-plane_number = p.Results.plane_number;
-n_frames = p.Results.n_frames;
+ds = p.Results.ds;
+plane = p.Results.plane;
+frames = p.Results.frames;
 
-if path == "None"
+if isempty(path)
     [file,path] = uigetfile({'*.h*';'.m*'},...
         'Select an extracted HDF5 or .mat file:');
     full_filepath = fullfile(path, file);
     if file == 0; error('No folder selected.'); end
+    if full_filepath(end-2:end) == ".mat"
+        Y_out = matfile(full_filepath);
+        return
+    end
 elseif isfile(path)
     full_filepath = path;
 elseif isfolder(path)
     files = dir(fullfile(path, '*_plane_*.h5'));
     if isempty(files); error('No plane files found in the given path: %s', path); end
-    if ~isempty(plane_number)
-        full_filepath = fullfile(path, files(plane_number).name);
+    if ~isempty(plane)
+        full_filepath = fullfile(path, files(plane).name);
     else
-        plane_number = input(sprintf('Please enter the plane number (1:%d): ', numel(files)));
-        if plane_number > numel(files); error("The given plane (%d) exceeds the number of planes found in this folder: %d\n", plane_number, numel(files)); end
-        if ischar(plane_number); plane_number = str2double(plane_number); end
-        if plane_number < 1 || plane_number > numel(files); error('Invalid plane number.'); end
-        full_filepath = fullfile(path, files(plane_number).name);
+        plane = input(sprintf('Please enter the plane number (1:%d): ', numel(files)));
+        if plane > numel(files); error("The given plane (%d) exceeds the number of planes found in this folder: %d\n", plane, numel(files)); end
+        if ischar(plane); plane = str2double(plane); end
+        if plane < 1 || plane > numel(files); error('Invalid plane number.'); end
+        full_filepath = fullfile(path, files(plane).name);
     end
 else
     error("No files found or selected.")
 end
 
-% Check if dataset_name was set via the default value
-if any(strcmp(p.UsingDefaults, 'dataset_name')); defaults=true; end
+if isempty(ds)
+    if is_valid_dataset(full_filepath, '/Y')
+        warning("No ds (dataset group name) provided, however h5 file contains /Y... Using this group.\n")
+        ds = "/Y";
+    elseif is_valid_dataset(full_filepath, '/mov')
+        warning("No ds (dataset group name) provided, however h5 file contains /mov... Using this group.\n")
+        ds = "/mov";
+    else
+        display_dataset_names(full_filepath);
+        ds = input('Please enter a valid dataset name (e.g. "/Y"): ', 's');
+    end
+end
 
-
-% Handle dataset_name
 try
-    data_info = h5info(full_filepath, dataset_name);
+    data_info = h5info(full_filepath, ds);
 catch ME
     if strcmp(ME.identifier, 'MATLAB:imagesci:h5info:libraryError') || contains(ME.message, 'Unable to find object')
-        fprintf('Dataset "%s" not found in file "%s".\n', dataset_name, plane_file);
-        display_dataset_names(plane_file);
-        dataset_name = input('Please enter a valid dataset name (e.g. "/Y"): ', 's');
-        if is_valid_group(dataset_name)
-            data_info = h5info(plane_file, dataset_name);
+        fprintf('Dataset "%s" not found in file "%s".\n', ds, full_filepath);
+        display_dataset_names(full_filepath);
+        ds = input('Please enter a valid dataset name (e.g. "/Y"): ', 's');
+        if is_valid_group(ds)
+            data_info = h5info(full_filepath, ds);
         else
-            fprintf('Given dataset_name "%s" is invalid. It must be quoted with a prepended \... try again.', dataset_name);
-            dataset_name = input('Please enter a valid dataset name (e.g. "/Y"): ', 's');
-            if ~is_valid_group(dataset_name)
+            fprintf('Given dataset_name "%s" is invalid. It must be quoted with a prepended \... try again.', ds);
+            ds = input('Please enter a valid dataset name (e.g. "/Y"): ', 's');
+            if ~is_valid_group(ds)
                 error("%s is still not a valid dataset name.");
             end
         end
@@ -98,30 +116,34 @@ catch ME
         rethrow(ME);
     end
 end
-
 data_size = data_info.Dataspace.Size;
-
-% Frames selection
-if ischar(n_frames) || isstring(n_frames)
-    if convertCharsToStrings(n_frames) == "all"
-        n_frames = data_size(end);
+if ischar(frames) || isstring(frames)
+    if convertCharsToStrings(frames) == "all"
+        frames = data_size(end);
+        slice_start = [ones(1, numel(data_size) - 1), 1];
+        slice_count = [data_size(1:end-1), data_size(end)];
     else
-        warning("Given argument for n_frames, if character/string, must be 'all', not: %s\n Try again!", n_frames);
-        n_frames = input(sprintf('Please enter the frames to read (1:%d): ', data_size(end)));
-        if max(n_frames) > data_size(end)
+        warning("Given argument for n_frames, if character/string, must be 'all', not: %s\n Try again!", frames);
+        frames = input(sprintf('Please enter the frames to read as a number (i.e. 4), slice (i.e. 1:%d) or "all": ', data_size(end)));
+        if max(frames) > data_size(end)
             error('Frames exceed the available range.');
         end
     end
+elseif isscalar(frames)
+    slice_start = [ones(1, numel(data_size) - 1), 1];
+    slice_count = [data_size(1:end-1), frames];
+elseif isvector(frames) && numel(frames) == 2
+    slice_start = [ones(1, numel(data_size) - 1), frames(1)];
+    slice_count = [data_size(1:end-1), frames(2) - frames(1) + 1];
+else
+    error('Invalid format for n_frames. It must be a scalar, a slice string, or a two-element array.');
 end
 
-slice_start = [ones(1, numel(data_size) - 1), n_frames(1)];
-slice_count = [data_size(1:end-1), n_frames];
-
 try
-    Y_out = h5read(full_filepath, dataset_name, slice_start, slice_count);
+    Y_out = h5read(full_filepath, ds, slice_start, slice_count);
 catch ME
     fprintf('\nError: %s\n', ME.message);
     Y_out = [];
 end
-fprintf("%d frames loaded.", n_frames);
+fprintf("%d frames loaded.\n", frames);
 end
