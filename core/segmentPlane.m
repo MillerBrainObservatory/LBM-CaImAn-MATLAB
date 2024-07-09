d2d77c7fd86a49ca98541b2e0df22101c12b4a9b
FlynnOConnell <FlynnOConnell@gmail.com>
2024-07-09 00:32:32 -0400

    chore: segmentPlane logs/params logging

--- core/segmentPlane.m
+++ core/segmentPlane.m
@@ -1,5 +1,5 @@
 function segmentPlane(data_path, save_path, varargin)
-% SEGMENTPLANE Segment imaging data using CaImAn for motion-corrected data.
+% Segment imaging data using CaImAn for motion-corrected data.
 %
 % This function applies the CaImAn algorithm to segment neurons from
 % motion-corrected, pre-processed and ROI re-assembled MAxiMuM data.
@@ -57,35 +57,44 @@ function segmentPlane(data_path, save_path, varargin)
 % See also ADDPATH, FULLFILE, DIR, LOAD, SAVEFAST
 
 p = inputParser;
-addRequired(p, 'data_path', @ischar);
-addRequired(p, 'save_path', @ischar);
-addParameter(p, 'dataset_name', "/mov", @(x) (ischar(x) || isstring(x)) && is_valid_group(x));
+addRequired(p, 'data_path', @(x) ischar(x) || isstring(x));
+addOptional(p, 'save_path', data_path, @(x) ischar(x) || isstring(x));
+addParameter(p, 'ds', "/Y", @(x) (ischar(x) || isstring(x)) && is_valid_group(x));
 addOptional(p, 'debug_flag', 0, @(x) isnumeric(x) || islogical(x));
 addParameter(p, 'overwrite', 1, @(x) isnumeric(x) || islogical(x));
 addParameter(p, 'num_cores', 1, @(x) isnumeric(x));
-addParameter(p, 'start_plane', 1, @(x) isnumeric(x) && x > 0);
-addParameter(p, 'end_plane', 1, @(x) isnumeric(x) && x >= p.Results.start_plane);
-addParameter(p, 'options', 1, @(x) isstruct(x));
+addParameter(p, 'start_plane', 1, @(x) isnumeric(x));
+addParameter(p, 'end_plane', 2, @(x) isnumeric(x) && x >= p.Results.start_plane);
+addParameter(p, 'options', {}, @(x) isstruct(x));
+addParameter(p, 'do_figures', 1, @(x) isnumeric(x) || islogical(x));
 parse(p, data_path, save_path, varargin{:});
 
 data_path = p.Results.data_path;
 save_path = p.Results.save_path;
-dataset_name = p.Results.dataset_name;
+ds = p.Results.ds;
 debug_flag = p.Results.debug_flag;
 overwrite = p.Results.overwrite;
 num_cores = p.Results.num_cores;
 start_plane = p.Results.start_plane;
 end_plane = p.Results.end_plane;
+do_figures = p.Results.do_figures;
 options = p.Results.options;
+
+data_path = convertStringsToChars(data_path);
+save_path = convertStringsToChars(save_path);
 
 % give access to CaImAn files
 [currpath, ~, ~] = fileparts(fullfile(mfilename('fullpath'))); % path to this script
 addpath(genpath(fullfile(currpath, '../packages/CaImAn_Utilities/CaImAn-MATLAB-master/CaImAn-MATLAB-master/')));
 addpath(genpath(fullfile(currpath, './utils')));
-addpath(genpath(fullfile(currpath, './io')));
+addpath(genpath(fullfile(currpath, './internal')));
 
-if ~isfolder(data_path); error("Data path:\n %s\n ..does not exist", data_path); end
-if debug_flag == 1; dir([data_path, '*.tif']); return; end
+if ~isfolder(data_path); error("Data path:\n %s\n ..does not exist, but should contain motion corrected HDF5 files.", data_path); end
+if debug_flag == 1 || debug_flag == true
+    disp("Files to process:")
+    dir([fullfile(data_path, '*.h*')])
+    return;
+end
 
 if isempty(save_path)
     warning("No save_path given. Saving data in data_path: %s\n", data_path);
@@ -108,42 +117,35 @@ if fid == -1
 else
     fprintf('Log file created: %s\n', log_full_path);
 end
-% closeCleanupObj = onCleanup(@() fclose(fid));
 
 %% Pull metadata from attributes attached to this group
 num_cores = max(num_cores, 23);
-fprintf(fid, '%s : Beginning registration with %d cores...\n\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), num_cores); tall=tic;
+log_message(fid, 'Beginning registration with %d cores...\n',num_cores);
 t_all=tic;
-first = true;
 for plane_idx = start_plane:end_plane
-    fprintf(fid, '%s : Beginning plane %d\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_idx);
+    log_message(fid, 'Beginning plane: %d\n',plane_idx);
     z_str = sprintf('plane_%d', plane_idx);
     plane_name = sprintf("%s//motion_corrected_%s.h5", data_path, z_str);
     plane_name_save = sprintf("%s//segmented_%s.h5", save_path, z_str);
-    if isfile(plane_name_save)
-        fprintf(fid, '%s : %s already exists.\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_name_save);
-        fprintf('%s : %s already exists.\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_name_save);
-
-        if overwrite
-            fprintf(fid, '%s : Parameter Overwrite=true. Deleting file: %s\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_name_save);
-            fprintf('%s : Parameter Overwrite=true. Deleting file: %s\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_name_save);
-            delete(plane_name_save)
-        else
-            fprintf(fid, '%s : Parameter Overwrite=true. Deleting file: %s\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_name_save);
-            fprintf('%s : Parameter Overwrite=true. Deleting file: %s\n', datestr(datetime('now'), 'yyyy_mm_dd_HH_MM_SS'), plane_name_save);
-
-        end
-    end
+    % if isfile(plane_name_save)
+    %     log_message(fid, '%s already exists.\n', plane_name_save);
+    %     if overwrite
+    %         log_message(fid, 'Parameter Overwrite=true. Deleting file: %s\n',plane_name_save);
+    %         delete(plane_name_save)
+    %     else
+    %         log_message(fid, 'Parameter Overwrite=true. Deleting file: %s\n',plane_name_save);
+    %     end
+    % end
 
     %% Attach metadata to attributes for this plane
     if plane_idx == start_plane
         metadata = read_h5_metadata(plane_name, '/');
         if isempty(fieldnames(metadata)); error("No metadata found for this filepath."); end
-        log_struct(metadata,'metadata', log_full_path,fid);
+        log_struct(fid,metadata,'metadata', log_full_path);
     end
 
     %% Load in data
-    data = double(h5read(plane_name, dataset_name));
+    data = double(h5read(plane_name, ds));
     % data = data - min(data(:));
 
     t_start = tic;
@@ -175,55 +177,55 @@ for plane_idx = start_plane:end_plane
     overlap = [30,30];
     patches = construct_patches(sizY(1:end-1),patch_size,overlap);
     K = ceil(600/numel(patches));  % number of components (neurons) to be found
+    
+    if isempty(options)
+        % Set caiman parameters
+        options = CNMFSetParms(...
+            'd1',d1,'d2',d2,...                         % dimensionality of the FOV
+            'deconv_method','constrained_foopsi',...    % neural activity deconvolution method
+            'temporal_iter',3,...                       % number of block-coordinate descent steps
+            'maxIter',15,...                            % number of NMF iterations during initialization
+            'spatial_method','regularized',...          % method for updating spatial components
+            'df_prctile',20,...                         % take the median of background fluorescence to compute baseline fluorescence
+            'p',p,...                                   % order of AR dynamics
+            'gSig',tau,...                              % half size of neuron
+            'merge_thr',merge_thresh,...                % merging threshold
+            'nb',1,...                                  % number of background components
+            'gnb',3,...
+            'min_SNR',min_SNR,...                       % minimum SNR threshold
+            'space_thresh',space_thresh ,...            % space correlation threshold
+            'decay_time',0.5,...                        % decay time of transients, GCaMP6s
+            'size_thr', sz, ...
+            'search_method','ellipse',...
+            'min_size', round(tau), ...                 % minimum size of ellipse axis (default: 3)
+            'max_size', 2*round(tau), ...               % maximum size of ellipse axis (default: 8)
+            'dist', dist, ...                           % expansion factor of ellipse (default: 3)
+            'max_size_thr',mx,...                       % maximum size of each component in pixels (default: 300)
+            'time_thresh',time_thresh,...
+            'min_size_thr',mn,...                       % minimum size of each component in pixels (default: 9)
+            'refine_flag',0,...
+            'rolling_length',ceil(frame_rate*5),...
+            'fr', frame_rate ...
+        );
+    end
 
-    % Set caiman parameters
-    options = CNMFSetParms(...
-        'd1',d1,'d2',d2,...                         % dimensionality of the FOV
-        'deconv_method','constrained_foopsi',...    % neural activity deconvolution method
-        'temporal_iter',3,...                       % number of block-coordinate descent steps
-        'maxIter',15,...                            % number of NMF iterations during initialization
-        'spatial_method','regularized',...          % method for updating spatial components
-        'df_prctile',20,...                         % take the median of background fluorescence to compute baseline fluorescence
-        'p',p,...                                   % order of AR dynamics
-        'gSig',tau,...                              % half size of neuron
-        'merge_thr',merge_thresh,...                % merging threshold
-        'nb',1,...                                  % number of background components
-        'gnb',3,...
-        'min_SNR',min_SNR,...                       % minimum SNR threshold
-        'space_thresh',space_thresh ,...            % space correlation threshold
-        'decay_time',0.5,...                        % decay time of transients, GCaMP6s
-        'size_thr', sz, ...
-        'search_method','ellipse',...
-        'min_size', round(tau), ...                 % minimum size of ellipse axis (default: 3)
-        'max_size', 2*round(tau), ...               % maximum size of ellipse axis (default: 8)
-        'dist', dist, ...                           % expansion factor of ellipse (default: 3)
-        'max_size_thr',mx,...                       % maximum size of each component in pixels (default: 300)
-        'time_thresh',time_thresh,...
-        'min_size_thr',mn,...                       % minimum size of each component in pixels (default: 9)
-        'refine_flag',0,...
-        'rolling_length',ceil(frame_rate*5),...
-        'fr', frame_rate ...
-    );
-
-    fprintf(fid, "%s : Data loaded in. This process took: %0.2f seconds.\nBeginning CNMF.\n\n",datetime("now"), toc(t_start));
-    fprintf("%s : Data loaded in. This process took: %0.2f seconds.\nBeginning CNMF with the following parameters:\n\n",datetime("now"), toc(t_start));
-    log_struct(options,'CNMF Parameters', log_full_path, fid);
-    disp(options);
+    log_message(fid, "Data loaded in. This process took: %0.2f seconds.\nBeginning CNMF.\n\n", toc(t_start));
+    log_message(fid, "--------------------------------------------------\n");
+    log_struct(fid,options,'CNMF Parameters', log_full_path);
+    log_message(fid, "--------------------------------------------------\n");
 
     t_cnmf = tic;
-
-    [A,b,C,f,S,P,~,YrA] = run_CNMF_patches(data,K,patches,tau,p,options);
-    fprintf(fid, '%s : Initialized CNMF patches complete.  Process took: %.2f seconds\Classifying components ...',datetime("now"), toc(t_cnmf));
-    fprintf('%s : Initialized CNMF patches complete.  Process took: %.2f seconds\Classifying components ...',datetime("now"), toc(t_cnmf));
-
+    [A,b,C,f,~,P,~,YrA] = run_CNMF_patches(data,K,patches,tau,p,options);
+    log_message(fid, 'Initialized CNMF patches complete.  Process took: %.2f seconds\Classifying components ...',toc(t_cnmf));
+    log_message(fid, "--------------------------------------------------\n");
+    
     t_class = tic;
-    [rval_space,rval_time,max_pr,sizeA,keep0,~,traces] = classify_components_jeff(data,A,C,b,f,YrA,options);
-    fprintf(fid, '%s : Classification complete. Process took: %.2f seconds\Running spatial/temporal acceptance tests ... to dF/F ...', datetime("now"), toc(t_class));
-    fprintf('%s : Classification complete. Process took: %.2f seconds\Running spatial/temporal acceptance tests ... to dF/F ...', datetime("now"), toc(t_class));
-
+    [rval_space,~,~,sizeA,~,~,traces] = classify_components_jeff(data,A,C,b,f,YrA,options);
+    log_message(fid, 'Classification complete. Process took: %.2f seconds\Running spatial/temporal acceptance tests ... to dF/F ...', datetime("now"), toc(t_class));
+    log_message(fid, "--------------------------------------------------\n");
+    
     t_test = tic;
     Cn =  correlation_image(data);
-
     % Spatial acceptance test:
     ind_corr = (rval_space > space_thresh) & (sizeA >= options.min_size_thr) & (sizeA <= options.max_size_thr);
 
@@ -239,27 +241,29 @@ for plane_idx = start_plane:end_plane
     Km = size(C_keep,1); % total number of components
 
     rVals = rval_space(keep);
-    fprintf(fid, '%s : First CNMF iteration complete. Process took: %.2f seconds\nUpdating tamporal components... ...', datetime("now"), toc(t_test));
-
+    log_message(fid, 'Spatial acceptance and event exceptionality tests complete. Process took: %.2f seconds\nUpdating tamporal components...', toc(t_test));
+    log_message(fid, "--------------------------------------------------\n");
+    
     %% Update temporal components
     P.p = 0;
     options.nb = options.gnb;
 
     t_update = tic;
     [C_keep,f,~,~,R_keep] = update_temporal_components(reshape(data,d,T),A_keep,b,C_keep,f,P,options);
-    fprintf(fid, '%s : Temporal components updates. Process took: %.2f seconds\nDetrending from raw traces ...', datetime("now"), toc(t_update));
-
+    log_message(fid, 'Temporal components updates. Process took: %.2f seconds\nDetrending from raw traces ...', datetime("now"), toc(t_update));
+    log_message(fid, "--------------------------------------------------\n");
+    
     %% Detrend
     t_detrend = tic;
     if size(A_keep,2) < 2 % Calculate "raw" traces in terms of delta F/F0
-        [T_keep,F0] = detrend_df_f([A_keep,ones(d1*d2,1)],[b,ones(d1*d2,1)],[C_keep;ones(1,T)],[f;-min(min(min(data)))*ones(1,T)],[R_keep; ones(1,T)],options);
+        [T_keep,~] = detrend_df_f([A_keep,ones(d1*d2,1)],[b,ones(d1*d2,1)],[C_keep;ones(1,T)],[f;-min(min(min(data)))*ones(1,T)],[R_keep; ones(1,T)],options);
     else % total number of pixels
         % handle min(data) = 0
         F_dark = min(min(data(:)),eps);
-        if F_dark == 0;
+        if F_dark == 0
             F_dark = eps;
         end
-        [T_keep,F0] = detrend_df_f( ...
+        [T_keep,~] = detrend_df_f( ...
             A_keep, ...
             [b,ones(d1*d2,1)], ...
             C_keep, ...
@@ -269,12 +273,15 @@ for plane_idx = start_plane:end_plane
         );
     end
 
-    fprintf(fid, '%s : Traces detrended. Process took: %.2f seconds\nConverting to sparse matrix ...', datetime("now"), toc(t_detrend));
+    log_message(fid, 'Traces detrended. Process took: %.2f seconds\nConverting to sparse matrix ...',toc(t_detrend));
+    log_message(fid, "--------------------------------------------------\n");
+
     %% Convert sparse A matrix to full 3D matrix
     t_sparse = tic;
-    [Ac_keep,acx,acy,acm] = AtoAc(A_keep,tau,d1,d2);  % Ac_keep has dims. [2*tau+1,2*tau+1,K] where each element Ki is a 2D map centered on centroid of component acx(Ki),axy(Ki), and acm(Ki) = sum(sum(Ac_keep(:,:,Ki))
-    fprintf(fid, '%s : Created sparse component matrix. Process took: %.2f seconds\nSaving data ...', datetime("now"), toc(t_sparse));
-
+    [Ac_keep,acx,acy,acm] = AtoAc(A_keep,tau,d1,d2, fid);  % Ac_keep has dims. [2*tau+1,2*tau+1,K] where each element Ki is a 2D map centered on centroid of component acx(Ki),axy(Ki), and acm(Ki) = sum(sum(Ac_keep(:,:,Ki))
+    log_message(fid, 'Created sparse component matrix. Process took: %.2f seconds\nSaving data ...',toc(t_sparse));
+    log_message(fid, "--------------------------------------------------\n");
+    
     % Convert ouputs to single to reduce memory consumption
     Cn = single(Cn);
     C_keep = single(C_keep);
@@ -283,7 +290,7 @@ for plane_idx = start_plane:end_plane
 
     % Save data
     t_save = tic;
-    fprintf(fid, "%s : Writing data.\n\n", datestr(datetime('now'), 'yyyy_mm_dd:HH:MM:SS'));
+    log_message(fid, "Writing data to disk to:\n\n %s\n", plane_name_save);
     write_frames_to_h5(plane_name_save, T_keep, 2000, '/T_keep');
     write_frames_to_h5(plane_name_save, Ac_keep, 2000, '/Ac_keep');
     write_frames_to_h5(plane_name_save, C_keep, 2000, '/C_keep');
@@ -298,17 +305,14 @@ for plane_idx = start_plane:end_plane
     write_frames_to_h5(plane_name_save, acm, 2000, '/acm');
 
     write_metadata_h5(metadata, plane_name_save, '/');
-    fprintf(fid, "%s : Data saved. Elapsed time: %.2f seconds\n", datestr(datetime('now'), 'yyyy_mm_dd:HH:MM:SS'), toc(t_save)/60);
-
+    log_message(fid, "Data saved. Elapsed time: %.2f seconds\n",toc(t_save)/60);
     % savefast(fullfile(save_path, ['caiman_output_plane_' num2str(plane_idx) '.mat']),'T_keep','Ac_keep','C_keep','Km','rVals','Ym','Cn','b','f','acx','acy','acm')
-    fprintf(fid, '%s : Data saved. Process took: %.2f seconds\n', datetime("now"), toc(t_save));
-    fprintf(fid, '%s : Plane complete. Process took: %.2f seconds\nBeginning next plane ...', datetime("now"), toc(t_start));
 end
 
-fprintf(fid, '%s : Routine complete. Total Completion time: %.2f hours\n', datetime("now"), toc(t_all)./3600);
+fprintf(fid, 'Routine complete. Total Completion time: %.2f hours\n', toc(t_all)./3600);
 end
 
-function [Ac_keep,acx,acy,acm] = AtoAc(A_keep,tau,d1,d2)
+function [Ac_keep,acx,acy,acm] = AtoAc(A_keep,tau,d1,d2,fid)
 %% Convert the sparse matrix A_keep to a full 3D matrix that can be saved to hdf5
 tau = tau(1);
 x = 1:d2;
@@ -319,9 +323,13 @@ Ac_keep = zeros(4*tau+1,4*tau+1,size(A_keep,2),'single');
 acx = zeros(1,size(A_keep,2));
 acy = acx;
 acm = acx;
-
-parfor ijk = 1:size(A_keep,2)
-
+log_message(fid, "--------------------------------------------------\n");
+log_message(fid,"Converting from sparse to full 3D component matrix")
+sd = size(A_keep, 2);
+tack = tic;
+parfor ijk = 1:sd
+    tijk = tic;
+    log_message(fid, "Iteration %d of %d -------- \n", ijk, sd);
     AOI = reshape(single(full(A_keep(:,ijk))),d1,d2);
     cx = round(trapz(trapz(X.*AOI))./trapz(trapz(AOI)));
     cy = round(trapz(trapz(Y.*AOI))./trapz(trapz(AOI)));
@@ -338,5 +346,8 @@ parfor ijk = 1:size(A_keep,2)
     AOIc = nan(4*tau+1,4*tau+1);
     AOIc(1:(ey-sy+1),1:(ex-sx+1)) = AOI(sy:ey,sx:ex);
     Ac_keep(:,:,ijk) = single(AOIc);
+    log_message(fid, 'Data converted to sparse. Process took: %.2f seconds.\n',toc(tijk));
 end
+log_message(fid, 'Data conversion complete. Process took: %.2f minutes.\n',toc(tack) / 60);
+
 end
