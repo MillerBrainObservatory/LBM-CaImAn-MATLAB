778bee99a5b1c038909980822ccfe73e79eac6b5
FlynnOConnell <FlynnOConnell@gmail.com>
2024-07-09 00:30:01 -0400

    feat: log without file identifier to command window

--- core/internal/log_message.m
+++ core/internal/log_message.m
@@ -3,13 +3,17 @@ function log_message(fid, msg, varargin)
     %
     % Parameters
     % ----------
-    % fid : file identifier
-    %     The file identifier for the log file.
+    % fid : file identifier (optional)
+    %     The file identifier for the log file. If not provided, logs only to the command window.
     % msg : char
     %     The message to log, with formatting placeholders.
     % varargin : any
     %     Additional arguments to format the message.
-    
+    if (ischar(fid) || isstring(fid))
+        fid = 1; % Default to command window
+        msg = fid;
+    end
+
     timestamp = datestr(datetime('now'), 'yyyy_mm_dd-HH:MM:SS');
 
     % Sanitize varargin to escape backslashes, but allow \n
@@ -22,7 +26,9 @@ function log_message(fid, msg, varargin)
     log_window_msg = sprintf('%s', sprintf(msg, varargin{:}));
     log_file_msg = sprintf('%s : %s', timestamp, log_window_msg);
 
-    fprintf(fid, '%s', log_file_msg);
+    if fid ~= 1
+        fprintf(fid, '%s', log_file_msg);
+    end
 
     fprintf('%s', log_window_msg);
 end
