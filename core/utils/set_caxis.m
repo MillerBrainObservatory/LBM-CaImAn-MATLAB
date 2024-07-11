033ea6cb8248774bca1e31a3543ce40450be3bef
Flynn OConnell <flynnoconnell@gmail.com>
2024-07-11 01:09:15 -0400

    docs: move scale functions to public

--- core/utils/set_caxis.m
+++ core/utils/set_caxis.m
@@ -1,4 +1,16 @@
 function set_caxis(stack)
+% Set the color axis limits for an image stack.
+%
+% This function sets the color axis limits of the current figure based on
+% the cumulative distribution function (CDF) of pixel values in the first
+% image of the provided stack. The limits are set to the 0.5th and 99.5th
+% percentiles to improve the contrast of the displayed images.
+%
+% Parameters
+% ----------
+% stack : 3D array
+%     The input image stack (height x width x frames).
+
 [counts, grayLevels] = hist(double(reshape(stack(:,:,1),1,[])),50);
 cdf = cumsum(counts);
 cdf = cdf / numel(stack(:,:,1));
