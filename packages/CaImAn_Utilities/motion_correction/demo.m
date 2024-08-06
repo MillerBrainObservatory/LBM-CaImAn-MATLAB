f58b5dbd6c9fe4ff736655e2119c8c43230ce7de
FlynnOConnell <FlynnOConnell@gmail.com>
2024-08-06 17:55:04 -0400

    chore: fix a few more caiman bugs

--- packages/CaImAn_Utilities/motion_correction/demo.m
+++ packages/CaImAn_Utilities/motion_correction/demo.m
@@ -1,49 +1,41 @@
 clear; clc;
 % gcp;
 
-parent_path = fullfile("C:/Users/RBO/Documents/data/high_res/v1.8/extracted/");
-data_path = fullfile(parent_path, "extracted_plane_1.h5");
+parent_path = fullfile("D:\DATA\2024-07-31_GCaMP8s_mk717\mk717_3umpx_10p59hz_224pxby732px_2mroi_350mw_50to550umdeep_00001");
+data_path = fullfile(parent_path, "extracted_plane_14.h5");
 metadata = read_h5_metadata(data_path);
-Y = single(read_plane(data_path)); % convert to single precision 
+Y = single(h5read(data_path, '/Y')); % convert to single precision 
+Y = Y(1:metadata.roi_height_px,:,:);
 Y = Y - min(Y(:));
 
-%% set parameters for rigid motion correction
+%%
+options_rigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'bin_width',200,'max_shift',15,'us_fac',50,'init_batch',200);
 
-options_rigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'bin_width',200,'max_shift',10,'us_fac',50,'init_batch',200);
+%% perform motion correction
 tic; [M1,shifts1,template1,options_rigid] = normcorre(Y,options_rigid); toc
 
+%% now try non-rigid motion correction (also in parallel)
 options_nonrigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'grid_size',[32,32],'mot_uf',4,'bin_width',200,'max_shift',15,'max_dev',3,'us_fac',50,'init_batch',200);
 tic; [M2,shifts2,template2,options_nonrigid] = normcorre_batch(Y,options_nonrigid); toc
+nnY = quantile(Y(:),0.005);
+mmY = quantile(Y(:),0.995);
 
-% cY:           correlation coefficient of each frame with the mean
-% mY:           mean image
-% ng:           norm of gradient of mean image
-
-[cY,mY,~] = motion_metrics(Y,10);
-[cM1,mM1,~] = motion_metrics(M1,10);
-[cM2,mM2,~] = motion_metrics(M2,10);
+[cY,mY,vY] = motion_metrics(Y,10);
+[cM1,mM1,vM1] = motion_metrics(M1,10);
+[cM2,mM2,vM2] = motion_metrics(M2,10);
 T = length(cY);
 
-f = figure('Visible', 'off', 'Units', 'normalized', 'OuterPosition', [0 0 1 1]);
-ax1 = subplot(2, 3, 1); imagesc(mY); axis equal; axis tight; axis off; 
-title('mean raw data', 'fontsize', 10, 'fontweight', 'bold');
-
-ax2 = subplot(2, 3, 2); imagesc(mM1); axis equal; axis tight; axis off; 
-title('mean rigid corrected', 'fontsize', 10, 'fontweight', 'bold');
-ax3 = subplot(2, 3, 3); imagesc(mM2); axis equal; axis tight; axis off; 
-title('mean non-rigid corrected', 'fontsize', 10, 'fontweight', 'bold');
-subplot(2, 3, 4); plot(1:T, cY, 1:T, cM1, 1:T, cM2); legend('raw data', 'rigid', 'non-rigid'); 
-title('correlation coefficients', 'fontsize', 10, 'fontweight', 'bold');
-subplot(2, 3, 5); scatter(cY, cM1); hold on; 
-plot([0.9 * min(cY), 1.05 * max(cM1)], [0.9 * min(cY), 1.05 * max(cM1)], '--r'); axis square;
-xlabel('raw data', 'fontsize', 10, 'fontweight', 'bold'); ylabel('rigid corrected', 'fontsize', 10, 'fontweight', 'bold');
-subplot(2, 3, 6); scatter(cM1, cM2); hold on; 
-plot([0.9 * min(cY), 1.05 * max(cM1)], [0.9 * min(cY), 1.05 * max(cM1)], '--r'); axis square;
-xlabel('rigid corrected', 'fontsize', 10, 'fontweight', 'bold'); ylabel('non-rigid corrected', 'fontsize', 10, 'fontweight', 'bold');
-linkaxes([ax1, ax2, ax3], 'xy');
-exportgraphics(f, fullfile(parent_path,"metrics.png"), 'Resolution', 600,'BackgroundColor','k');
-close(f);
-
+%% plot metrics
+figure;
+    ax1 = subplot(2,3,1); imagesc(mY,[nnY,mmY]);  axis equal; axis tight; axis off; title('mean raw data','fontsize',14,'fontweight','bold')
+    ax2 = subplot(2,3,2); imagesc(mM1,[nnY,mmY]);  axis equal; axis tight; axis off; title('mean rigid corrected','fontsize',14,'fontweight','bold')
+    ax3 = subplot(2,3,3); imagesc(mM2,[nnY,mmY]); axis equal; axis tight; axis off; title('mean non-rigid corrected','fontsize',14,'fontweight','bold')
+    subplot(2,3,4); plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
+    subplot(2,3,5); scatter(cY,cM1); hold on; plot([0.9*min(cY),1.05*max(cM1)],[0.9*min(cY),1.05*max(cM1)],'--r'); axis square;
+        xlabel('raw data','fontsize',14,'fontweight','bold'); ylabel('rigid corrected','fontsize',14,'fontweight','bold');
+    subplot(2,3,6); scatter(cM1,cM2); hold on; plot([0.9*min(cY),1.05*max(cM1)],[0.9*min(cY),1.05*max(cM1)],'--r'); axis square;
+        xlabel('rigid corrected','fontsize',14,'fontweight','bold'); ylabel('non-rigid corrected','fontsize',14,'fontweight','bold');
+    linkaxes([ax1,ax2,ax3],'xy')
 %% plot shifts        
 
 shifts_r = squeeze(cat(3,shifts1(:).shifts));
@@ -52,46 +44,28 @@ shifts_nr = reshape(shifts_nr,[],ndims(Y)-1,T);
 shifts_x = squeeze(shifts_nr(:,1,:))';
 shifts_y = squeeze(shifts_nr(:,2,:))';
 
-f = figure("Visible","off");
-    ax1 = subplot(311);
-    plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid');
-    title('correlation coefficients','fontsize',8,'fontweight','bold')
-            set(gca,'Xtick',[])
-    ax2 = subplot(312);
-    plot(shifts_x); hold on; plot(shifts_r(:,1),'--r','linewidth',2);
-    title('displacements along x','fontsize',8,'fontweight','bold')
-            set(gca,'Xtick',[])
-    ax3 = subplot(313);
-    plot(shifts_y); hold on; plot(shifts_r(:,2),'--r','linewidth',2);
-    title('displacements along y','fontsize',8,'fontweight','bold')
-            xlabel('timestep','fontsize',8,'fontweight','bold')
-linkaxes([ax1,ax2,ax3],'x')
-exportgraphics(f, fullfile(parent_path,"shifts.png"), 'Resolution', 600, 'BackgroundColor', 'k');
-close(f);
+patch_id = 1:size(shifts_x,2);
+str = strtrim(cellstr(int2str(patch_id.')));
+str = cellfun(@(x) ['patch # ',x],str,'un',0);
 
-%% save movie with videowriter object
-% Create a VideoWriter object
-clc; close(gcf);
-video_filename = fullfile(parent_path, 'registration_results.avi');
-v = VideoWriter(video_filename);
-v.FrameRate = metadata.frame_rate;
-open(v);
+figure;
+    ax1 = subplot(311); plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
+            set(gca,'Xtick',[])
+    ax2 = subplot(312); plot(shifts_x); hold on; plot(shifts_r(:,1),'--k','linewidth',2); title('displacements along x','fontsize',14,'fontweight','bold')
+            set(gca,'Xtick',[])
+    ax3 = subplot(313); plot(shifts_y); hold on; plot(shifts_r(:,2),'--k','linewidth',2); title('displacements along y','fontsize',14,'fontweight','bold')
+            xlabel('timestep','fontsize',14,'fontweight','bold')
+    linkaxes([ax1,ax2,ax3],'x')
 
-fig = figure;
-for t = 1:T
-    subplot(121); imagesc(Y(:,:,t));
-    xlabel('raw data', 'fontsize', 8, 'fontweight', 'bold'); axis equal; axis tight;
-    title(sprintf('Frame %i out of %i', t, T), 'fontweight', 'bold', 'fontsize', 8);
-    colormap('gray');
-    
-    subplot(122); imagesc(M2(:,:,t));
-    xlabel('non-rigid corrected', 'fontsize', 8, 'fontweight', 'bold'); axis equal; axis tight;
-    title(sprintf('Frame %i out of %i', t, T), 'fontweight', 'bold', 'fontsize', 8);
-    colormap('gray');
-    
-    set(gca, 'XTick', [], 'YTick', []);
-    frame = getframe(fig);
-    writeVideo(v, frame);
+%% plot a movie with the results
+
+figure;
+for t = 1:1:T
+    subplot(121);imagesc(Y(:,:,t),[nnY,mmY]); xlabel('raw data','fontsize',14,'fontweight','bold'); axis equal; axis tight;
+    title(sprintf('Frame %i out of %i',t,T),'fontweight','bold','fontsize',14); colormap('bone')
+    subplot(122);imagesc(M2(:,:,t),[nnY,mmY]); xlabel('non-rigid corrected','fontsize',14,'fontweight','bold'); axis equal; axis tight;
+    title(sprintf('Frame %i out of %i',t,T),'fontweight','bold','fontsize',14); colormap('bone')
+    set(gca,'XTick',[],'YTick',[]);
+    drawnow;
+    pause(0.02);
 end
-close('all');
-fprintf('Video saved to %s\n', video_filename);
